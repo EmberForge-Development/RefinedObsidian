a14ec04888f1e91b69345dcfbb567480722adb59
Antony Blem <antony.blem@icloud.com>
2025-03-16 15:19:53 +0100

    1.20.1

--- src/main/java/dev/emberforge/RefinedObsidian.java
+++ src/main/java/dev/emberforge/RefinedObsidian.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 
 public class RefinedObsidian implements ModInitializer {
 	public static final String MOD_ID = "refinedobsidian";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
