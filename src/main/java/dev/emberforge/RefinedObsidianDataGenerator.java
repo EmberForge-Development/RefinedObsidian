f33123b2bf26732dd8e9a486b40124922455a96c
Antony Blem <antony.blem@icloud.com>
2025-03-16 14:59:03 +0100

    1.21.4

---
+++ src/main/java/dev/emberforge/RefinedObsidianDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.emberforge;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RefinedObsidianDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
